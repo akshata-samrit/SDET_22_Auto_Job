c9ca2448ba588e447bd296aa29fbc28b1a3013ce
akshata-samrit <akshatabadwaik@gmail.com>
2021-10-19 13:23:46 +0530

    Hi Commit

---
+++ CRM_Maven_Project/src/test/java/com/crm/contactTest/ContactTest.java
@@ -0,0 +1,18 @@
+package com.crm.contactTest;
+
+import org.testng.annotations.Test;
+
+public class ContactTest 
+{
+	@Test
+	public void createContactTest()
+	{
+		System.out.println("Execute Create Contact Test");
+	}
+	
+	@Test
+	public void editContactTest()
+	{
+		System.out.println("Execute Edit Contact Test");
+	}
+}
---
+++ CRM_Maven_Project/src/test/java/com/crm/orgtest/OrgTest.java
@@ -0,0 +1,20 @@
+package com.crm.orgtest;
+
+import org.testng.annotations.Test;
+
+public class OrgTest 
+{
+	@Test
+	public void createOrgTest()
+	{
+		System.out.println("Execute Create Org Test");
+		
+		String URL = System.getProperty("url");
+		System.out.println(URL);
+		
+		String BROWSER = System.getProperty("browser");
+		System.out.println(BROWSER);
+	}
+}
+
+
